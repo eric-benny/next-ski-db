8253a9539947f38cda9a9dab565c686f0bd46de1
eric-benny <59672346+eric-benny@users.noreply.github.com>
2022-10-19 19:49:50 -0400

    deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,4 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import Auth0Provider from "next-auth/providers/auth0";
 
 // Prisma adapter for NextAuth, optional and can be removed
