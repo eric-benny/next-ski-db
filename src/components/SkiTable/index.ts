b432aea3ed3f22cf7630c6ee522c3fbe6d77388a
eric-benny <59672346+eric-benny@users.noreply.github.com>
2023-03-28 10:33:40 -0400

    remove some emotion stuff and mui table

--- src/components/SkiTable/index.ts
+++ src/components/SkiTable/index.ts
@@ -1,2 +1,1 @@
-export { SkiTable } from './SkiTable'
 export { SkiTableNew } from './SkiTableNew'
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -19,14 +19,32 @@ import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { prisma } from "~/server/db";
 import { getAuth } from "@clerk/nextjs/server";
 
+type CreateContextOptions = Record<string, never>;
+
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * it from here.
+ *
+ * Examples of things you may need it for:
+ * - testing, so we don't have to mock Next.js' req/res
+ * - tRPC's `createSSGHelpers`, where we don't have req/res
+ *
+ * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
+ */
+const createInnerTRPCContext = (opts: CreateContextOptions) => {
+  return {
+    prisma,
+  };
+};
+
 /**
  * This is the actual context you will use in your router. It will be used to process every request
  * that goes through your tRPC endpoint.
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = (opts: CreateNextContextOptions) => {
-  const { req } = opts;
+export const createTRPCContext = (_opts: CreateNextContextOptions) => {
+  const { req } = _opts;
   const sesh = getAuth(req);
 
   const userId = sesh.userId;
@@ -40,9 +58,11 @@ export const createTRPCContext = (opts: CreateNextContextOptions) => {
 /**
  * 2. INITIALIZATION
  *
- * This is where the tRPC API is initialized, connecting the context and transformer.
+ * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
+ * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
+ * errors on the backend.
  */
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
@@ -82,19 +102,3 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
-
-const enforceUserIsAuthed = t.middleware(async ({ ctx, next }) => {
-  if (!ctx.userId) {
-    throw new TRPCError({
-      code: "UNAUTHORIZED",
-    });
-  }
-
-  return next({
-    ctx: {
-      userId: ctx.userId,
-    },
-  });
-});
-
-export const privateProcedure = t.procedure.use(enforceUserIsAuthed);
