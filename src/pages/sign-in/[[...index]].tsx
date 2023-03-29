ca4197d45240f0d7e6c9aa7af144795c3f8e1c24
eric-benny <59672346+eric-benny@users.noreply.github.com>
2023-03-28 21:07:24 -0400

    style clerk

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -5,7 +5,18 @@ const SignInPage = () => (
   <>
     <Navbar />
     <div className="flex justify-center">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl="/sign-up"
+        appearance={{
+          variables: {
+            colorPrimary: "#DB252E",
+            colorText: "black",
+            fontFamily: "Roboto",
+          },
+        }}
+      />
     </div>
   </>
 );
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -5,7 +5,18 @@ const SignUpPage = () => (
   <>
     <Navbar />
     <div className="flex justify-center">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+      <SignUp
+        path="/sign-up"
+        routing="path"
+        signInUrl="/sign-in"
+        appearance={{
+          variables: {
+            colorPrimary: "#DB252E",
+            colorText: "black",
+            fontFamily: "Roboto",
+          },
+        }}
+      />
     </div>
   </>
 );
